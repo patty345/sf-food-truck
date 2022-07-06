cd8093eb6eaff9a3a1b975c528d0b90f589691f6
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-07-06 12:06:35 -0500

    initial files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "./App.css";
+import truckSites from "./data/truck-sites.json";
 
 function App() {
+  // const filteredTruck = truckSites.filter(truck => truck.)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer
+      center={[37.774929, -122.419418]}
+      zoom={13}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {truckSites.map((truck) => (
+        <Marker
+          key={truck.objectid}
+          
+          position={[+truck.location.latitude, +truck.location.longitude]}
+        />
+      ))}
+    </MapContainer>
   );
 }
 
